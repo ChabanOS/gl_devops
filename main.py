7addf018cee2399782e4917865e25667587acff4
Oleksii Chaban <oleksii.chaban@flex.com>
2020-02-27 12:42:05 +0200

    add comments to the main script

--- main.py
+++ main.py
@@ -1,8 +1,16 @@
+"""
+    This script was written by Oleksii Chaban for DevOps GL ProCamp
+    Feb 2020
+"""
+
 import psutil
 from psutil._common import bytes2human
 import sys
 
 
+""" This function checks that the argument which was provided to the script is correct   
+    and stops the script in case the argument is wrong. 
+    In case you need to add a new argument, modify allowed_args list"""
 def check_argument():
     allowed_args = ['cpu', 'mem']
     if len(sys.argv) == 1 or len(sys.argv) > 2 or not sys.argv[1].lower() in allowed_args:
@@ -12,14 +20,21 @@ def check_argument():
         return sys.argv[1].lower()
 
 
+""" print_metric function prints formatted output to the console.
+    This function takes two arguments:
+    data - metric data
+    metrics - an argument provided by the user to the script"""
 def print_metric(data, metric):
     for name in data._fields:
         value = getattr(data, name)
+
+        # Shows a value in a human-readable format for Memory metric (B, Kb, Mb, Gb)
         if metric == 'mem':
             value = bytes2human(value)
+
         print("{:15}{:>10}".format(name.capitalize(), value))
 
-
+# The main function in the script, it executes all other functions and prints output to the user
 def main():
     metric = check_argument()
     if metric == 'cpu':
